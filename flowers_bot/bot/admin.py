497429ba436a1fa62eb13b10e252f14c3976e99b
Elizaveta Shvedova <elizabethsweden@icloud.com>
2023-08-15 23:34:30 +0300

    Зарегистрировала модель Client в админке

--- flowers_bot/bot/admin.py
+++ flowers_bot/bot/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Client
 
 # Register your models here.
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    search_fields = ['first_name','last_name','phone','address']
+    list_filter = ['is_florist','is_courier']
+    list_display = ['first_name','last_name','phone']
